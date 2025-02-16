4bd3a7207d81c9666636face27cb42225abb8280
Anantha Krishna B <157831286+akbted@users.noreply.github.com>
2025-02-16 22:18:12 +0530

    Enhanced renaming script to ensure images and labels are renamed simultaneously and check for matching names

--- Helper/renaming.py
+++ Helper/renaming.py
@@ -19,26 +19,44 @@ def rename_files(image_folder, label_folder, base_name):
     None
     """
 
-    # Rename images
-    for count, filename in enumerate(os.listdir(image_folder), start=1):
-        file_extension = os.path.splitext(filename)[1]
-        new_name = f"{base_name}_{count}{file_extension}"
-        os.rename(
-            os.path.join(image_folder, filename), os.path.join(image_folder, new_name)
+    # List all the files in the directory
+    image_files = sorted([f for f in os.listdir(image_folder) if f.endswith(".jpg")])
+    label_files = sorted([f for f in os.listdir(label_folder) if f.endswith(".txt")])
+
+    # Check if the number of images and labels are the same
+    if len(image_files) != len(label_files):
+        print("Number of images and labels are not the same")
+    else:
+        print(
+            "Number of images and labels are the same - Starting the Renaming Process"
         )
 
-    # Rename labels
-    for count, filename in enumerate(os.listdir(label_folder), start=1):
-        file_extension = os.path.splitext(filename)[1]
-        new_name = f"{base_name}_{count}{file_extension}"
-        os.rename(
-            os.path.join(label_folder, filename), os.path.join(label_folder, new_name)
-        )
+    # Renaming the images and labels simultaneously with the same name
+    for i in range(len(image_files)):
+        # Check if image and label have the same name
+        if image_files[i].split(".")[0] == label_files[i].split(".")[0]:
+            new_img_name = base_name + "_" + str(i) + ".jpg"
+            new_label_name = base_name + "_" + str(i) + ".txt"
+            os.rename(
+                os.path.join(image_folder, image_files[i]),
+                os.path.join(image_folder, new_img_name),
+            )
+            os.rename(
+                os.path.join(label_folder, label_files[i]),
+                os.path.join(label_folder, new_label_name),
+            )
+        else:
+            print(f"{image_files[i]} and {label_files[i]} do not have the same name")
+
     print("COMPLETED RE-NAMING")
 
 
 # Example usage
-image_folder = "-----"
-label_folder = "----"
-base_name = "------"  # MY ID
+image_folder = (
+    "/Users/akb/Desktop/CV_GenAi/Project/visionaid/Data/anantha.k/train/images"
+)
+label_folder = (
+    "/Users/akb/Desktop/CV_GenAi/Project/visionaid/Data/anantha.k/train/labels"
+)
+base_name = "KNCVB10065"  # MY ID
 rename_files(image_folder, label_folder, base_name)
